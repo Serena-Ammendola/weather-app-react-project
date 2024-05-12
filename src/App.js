a19500e84c7ee8a9cfef0fa1eb518fe8abdda9b9
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-05-12 23:23:36 +0100

    added the github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ export default function App() {
   return (
     <div className="App">
       <h1>My weather app</h1>
+      <div>
+        {" "}
+        This project is
+        <a
+          href="https://github.com/Serena-Ammendola/weather-app-react-project"
+          target="_blank"
+        >
+          {" "}
+          opened source on GitHub
+        </a>
+      </div>
     </div>
   );
 }
