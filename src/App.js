8911437af7805ab340a2a45dbc09eedde017abb9
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-05-13 21:46:39 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>My weather app</h1>
-      <div>
-        {" "}
-        This project is
-        <a
-          href="https://github.com/Serena-Ammendola/weather-app-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <h1>My weather app</h1>
+        <div>
           {" "}
-          opened source on GitHub
-        </a>
+          This project is
+          <a
+            href="https://github.com/Serena-Ammendola/weather-app-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            opened source on GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
